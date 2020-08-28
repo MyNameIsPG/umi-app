d511e10f311f28622aad4af8cbc6dc8ea31269a7
yidianhengji <493871850@qq.com>
2020-08-28 16:50:47 +0800

    11

---
+++ .umirc.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'umi';
+
+export default defineConfig({
+  nodeModulesTransform: {
+    type: 'none',
+  },
+  routes: [
+    {
+      path: '/home', component: '@/layouts', routes: [
+        { name: 'login', path: '/home/test', component: '@/pages/test' },
+        { name: 'login', path: '/home/test2', component: '@/pages/test2' },
+      ]
+    },
+    { path: '/login', component: '@/pages/login' }
+  ],
+  title: '链接图标'
+});
